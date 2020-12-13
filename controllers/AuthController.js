26dddb79f6f3ac781c3bca41cc75041c90c3adaa
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-12-13 16:18:25 +0100

    small comment added

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,25 +6,35 @@ var jwt = require("jsonwebtoken");
 exports.authRegister = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
-  // Field Validation
+  // FİELD VALİDATİON
+  // Verileri doğrulamak için kullanılır. MESELA email mi değil mi gibi ? örnek olarak email içine @ yapmayı unutmuş olabilir. o zaman hata verir
+
   const validationErr = validationResult(req);
   if (validationErr?.errors?.length > 0) {
-    return res.status(400).json({ errors: validationErr.array() });
+    return res
+      .status(400)
+      .json({ errors: validationErr.array() });
   }
 
-  // User exist check
-  const userData = await User.findOne({ email });
-  if (userData) {
+  // USER EXİST CHECK 
+  // email kontrolü yapıyoruz. bakalım iki aynı email kayıt oluyor mu ?
+  
+  const userData = await User.findOne({ email }); // { email : mail@mail.com } şeklinde yazılıyor ama ES6 ile bu zorunluluk kalktı.
+    if (userData) {
     return res
       .status(400)
       .json({ errors: [{ message: "User already exists!!" }] });
   }
-
-  // Password hash
+   
+  // PASSWORD HASH
+  //Kriptolamaya verilen tanımlama. bir resmi sayıyı veya objeyi belirli bir algoritmadan geçirip anlamsız sayılar hafrler bütünü haline getirmek
+  
   const salt = await bcrypt.genSalt(10);
+  // const salt = bcrypt.genSaltSync(10);  => yukardaki gibi kullanmak(ASYNC_AWAİT) yerine bu satırdaki gibi de kullanılabilir
   const newPassword = await bcrypt.hash(password, salt);
+  // const newPassword = bcrypt.hashSync(password, salt);   => yukardaki gibi kullanmak(ASYNC_AWAİT) yerine bu satırdaki gibi de kullanılabilir
 
-  // Save User
+  // SAVE USER
   const user = new User({
     firstName,
     lastName,
@@ -37,36 +47,53 @@ exports.authRegister = async (req, res) => {
   res.send("Register Completed.");
 };
 
+
+
+
 exports.authLogin = async (req, res) => {
   const { email, password } = req.body;
 
-  // Field Validation
+    // FİELD VALİDATİON
+
   const validationErr = validationResult(req);
   if (validationErr?.errors?.length > 0) {
-    return res.status(400).json({ errors: validationErr.array() });
-  }
-
-  // User exist check
-  const userData = await User.findOne({ email });
-  if (!userData) {
     return res
       .status(400)
-      .json({ errors: [{ message: "User doesn't exists!!" }] });
+      .json({ errors: validationErr.array() });
   }
 
-  // Password compare
-  const isPasswordMatch = await bcrypt.compare(password, userData.password);
-  if (!isPasswordMatch) {
+  // USER EXİST CHECK  - Kullanıcı var mı ?
+  
+  const userData = await User.findOne({ email }); // { email : mail@mail.com } şeklinde yazılıyor ama ES6 ile bu zorunluluk kalktı.
+  
+  if (!userData) { // yukardakinden farklı olarak bu rda user yok ise error verecek. Baştaki ! o işe yarıyor.
+    return res
+    .status(400)
+      .json({ errors: [{ message: "User doesn't exists!!" }] });
+    }
+    
+ // PASSWORD COMPARE / Şifre Karşılaştırma
+ const isPasswordMatch = await bcrypt.compare(password, userData.password);
+  if (!isPasswordMatch) { // eğer matchh etmedi ise aşağıdaki hatayı dön.
     return res
       .status(400)
       .json({ errors: [{ message: "Invalid credentials" }] });
-  }
+      // buraya "Invalid credentials" yazılmasının seebebi. email veya pass hangisinin hataı olduğu söylenmiyor
+    }
 
-  // JSON WEB TOKEN - JWT
+    
+
+  // ***** Burası önemli çünkü artık eşleşen kişiye yetki verilecek.
+
+
+  // JSON WEB TOKEN - JWT ()
+  // Token = Jeton/Bilet => biz bu yetki ile login sayfasındaki kişiye bilet vermiş oluyoruz. bu token ı o yetki kontrolu yapılan yerlerde kullanacağız
+
   jwt.sign(
     { userData },
     process.env.JWT_SECRET_KEY,
-    { expiresIn: 3600 },
+    { expiresIn: 3600 }, 
+    // 3600 : 3600 = 24 saat demektir. Burda verilen token'a süre veriliyor. { expiresIn: "24h" }, => bu şekilde de yazılabilir
     (err, token) => {
       if (err) {
         return res.status(400).json({ errors: [{ message: "Unknown Error" }] });
