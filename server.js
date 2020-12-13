ecf8b9787f835f11764231e6dbc7ca56a37240d1
Talha-35 <67706755+Talha-35@users.noreply.github.com>
2020-12-13 17:57:09 +0100

    server updated (client page added)

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ connectDB();
 app.use(express.json());
 app.use("/api", router);
 
+
+//PRODUCTİON
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+// bu kısmı react yani client kısmını görsün diye yaptık
+app.get("*", (req,res) =>{
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+})
+}
+
 app.listen(port, () => {
   console.log(`I'm listening on port ${port}`);
 });
